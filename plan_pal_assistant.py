14e8012389bc4e0ce07c7f1de4f3aed4489a01c4
zohrehalavi <zohreh.alavi@gmail.com>
2024-03-09 13:16:07 -0500

    clean the code

--- plan_pal_assistant.py
+++ plan_pal_assistant.py
@@ -1,4 +1,6 @@
+import logging
 import os
+from logger import get_logger
 
 import openai
 import requests
@@ -15,138 +17,147 @@ from dotenv import load_dotenv
 
 import os
 
-client = OpenAI()
 GPT_MODEL = "gpt-3.5-turbo-0613"
 
-print(f"Current working directory: {os.getcwd()}")
 
-# Load environment variables from .env file
-load_dotenv(".env")
+class PlanPal:
+    def __init__(self):
+        self.logger = get_logger(self.__class__.__name__)
+        self.client = OpenAI()
+        # Load environment variables from .env file
+        load_dotenv(".env")
 
-# Ensure your OpenAI API key is loaded from the environment variables
-openai.api_key = os.getenv("OPENAI_API_KEY")
+        # Ensure your OpenAI API key is loaded from the environment variables
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.cal = MyCalendar()
 
+    def get_calendar_events(self, n):
+        print("called get_calendar_events function! ")
+        events = self.cal.get_events(n=n)
+        return json.dumps({"events": events})
 
-def get_calendar_events(n):
-    c = MyCalendar()
-    print("called get_calendar_events function! ")
-    events = c.get_events(n=n)
-    return json.dumps({"events": events})
+    def get_calendar_events_n_days(self, n_days):
+        print("called get_calendar_events function! ")
+        events = self.cal.get_events_n_days(n_days)
+        return json.dumps({"events": events})
 
+    def create_calendar_event(self, start_time, summary, duration=1, description=None, location=None):
+        details = self.cal.create_event(start_time, summary, duration, description, location)
+        print("called create_calendar_event function! ")
+        return json.dumps({"details": details})
 
-def get_calendar_events_n_days(n_days):
-    c = MyCalendar()
-    print("called get_calendar_events function! ")
-    events = c.get_events_n_days(n_days)
-    return json.dumps({"events": events})
+    def pretty_print_conversation(self, messages):
+        role_to_color = {
+            "system": "red",
+            "user": "green",
+            "assistant": "blue",
+            "tool": "magenta",
+        }
 
+        for message in messages:
+            if message['role'] == "system":
+                print(colored(f"system: {message['content']}\n", role_to_color[message["role"]]))
+            elif message['role'] == "user":
+                print(colored(f"user: {message['content']}\n", role_to_color[message["role"]]))
+            elif message['role'] == "assistant" and message.get("function_call"):
+                print(colored(f"assistant: {message['function_call']}\n", role_to_color[message["role"]]))
+            elif message['role'] == "assistant" and not message.get("function_call"):
+                print(colored(f"assistant: {message['content']}\n", role_to_color[message["role"]]))
+            elif message['role'] == "tool":
+                print(colored(f"function ({message['name']}): {message['content']}\n", role_to_color[message["role"]]))
 
-def create_calendar_event(start_time, summary, duration=1, description=None, location=None):
-    c = MyCalendar()
-    details = c.create_event(start_time, summary, duration, description, location)
-    print("called create_calendar_event function! ")
-    return json.dumps({"details": details})
+    @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
+    def chat_completion_request(self, messages, tools=None, tool_choice=None, model=GPT_MODEL):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + openai.api_key,
+        }
+        json_data = {"model": model, "messages": messages}
+        if tools is not None:
+            json_data.update({"tools": tools})
+        if tool_choice is not None:
+            json_data.update({"tool_choice": tool_choice})
+        try:
+            response = requests.post(
+                "https://api.openai.com/v1/chat/completions",
+                headers=headers,
+                json=json_data,
+            )
+            return response
+        except Exception as e:
+            print("Unable to generate ChatCompletion response")
+            print(f"Exception: {e}")
+            return e
 
+    def run_conversation(self, user_input, history:dict = None, max_history_messages=20):
+        self.logger.debug("user prompt", user_input)
 
-def pretty_print_conversation(messages):
-    role_to_color = {
-        "system": "red",
-        "user": "green",
-        "assistant": "blue",
-        "tool": "magenta",
-    }
+        local_timezone = datetime.now(pytz.timezone('America/New_York'))  # Replace with your time zone
+        current_datetime = local_timezone.strftime('%Y-%m-%d %H:%M:%S')
 
-    for message in messages:
-        if message['role'] == "system":
-            print(colored(f"system: {message['content']}\n", role_to_color[message["role"]]))
-        elif message['role'] == "user":
-            print(colored(f"user: {message['content']}\n", role_to_color[message["role"]]))
-        elif message['role'] == "assistant" and message.get("function_call"):
-            print(colored(f"assistant: {message['function_call']}\n", role_to_color[message["role"]]))
-        elif message['role'] == "assistant" and not message.get("function_call"):
-            print(colored(f"assistant: {message['content']}\n", role_to_color[message["role"]]))
-        elif message['role'] == "tool":
-            print(colored(f"function ({message['name']}): {message['content']}\n", role_to_color[message["role"]]))
+        # Step 1: send the conversation and available functions to the model
+        messages = [{"role": "system",
+                     "content": "Don't make assumptions about what values to plug into functions. Ask for "
+                                "clarification if a user request is ambiguous."},
+                    {"role": "system",
+                     "content": "The user must specify time and date in the request. Ask for time or date if it is not "
+                                "specified! Don't make time or date up"},
 
+                    {"role": "system",
+                     "content": f"Current Date and Time: {current_datetime}, Local Time Zone: {local_timezone}"
+                     }]
+        # If there's history, prepend it to the current session's messages
+        # If history is provided, integrate its messages at the beginning
+        if history:
+            # Limit the history to the last max_history_messages messages
+            limited_history = history['messages'][-max_history_messages:]
+            messages = limited_history + messages
 
-@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + openai.api_key,
-    }
-    json_data = {"model": model, "messages": messages}
-    if tools is not None:
-        json_data.update({"tools": tools})
-    if tool_choice is not None:
-        json_data.update({"tool_choice": tool_choice})
-    try:
-        response = requests.post(
-            "https://api.openai.com/v1/chat/completions",
-            headers=headers,
-            json=json_data,
+        # Append the new user input to the conversation
+        messages.append({"role": "user", "content": user_input})
+
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages,
+            tools=gpt_tools.tools,
+            temperature=0.0,
+            tool_choice="auto",  # auto is default, but we'll be explicit
         )
-        return response
-    except Exception as e:
-        print("Unable to generate ChatCompletion response")
-        print(f"Exception: {e}")
-        return e
+        response_message = response.choices[0].message
 
+        simulated_response = {"role": "assistant", "content": response_message}
+        messages.append(simulated_response)
 
-def run_conversation(user_input):
-    local_timezone = datetime.now(pytz.timezone('America/New_York'))  # Replace with your time zone
-    current_datetime = local_timezone.strftime('%Y-%m-%d %H:%M:%S')
+        self.pretty_print_conversation(messages)
 
-    # Step 1: send the conversation and available functions to the model
-    messages = [{"role": "system",
-                 "content": "Don't make assumptions about what values to plug into functions. Ask for "
-                            "clarification if a user request is ambiguous."},
-                {"role": "system",
-                 "content": "The user must specify time and date in the request. Ask for time or date if it is not "
-                            "specified! Don't make time or date up"},
+        tool_calls = response_message.tool_calls
+        self.logger.debug("tool_calls", tool_calls)
 
-                {"role": "system",
-                 "content": f"Current Date and Time: {current_datetime}, Local Time Zone: {local_timezone}"
-                 },
-                {"role": "user",
-                 "content": user_input}]
+        if not tool_calls:
+            self.pretty_print_conversation(messages)
+            return messages
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=messages,
-        tools=gpt_tools.tools,
-        temperature=0,
-        tool_choice="auto",  # auto is default, but we'll be explicit
-    )
-    response_message = response.choices[0].message
+        # Step 2: check if the model wanted to call a function
 
-    # return response_message.content.__str__()
-
-    pretty_print_conversation(messages)
-
-    tool_calls = response_message.tool_calls
-    print("tool_calls", tool_calls)
-    # Step 2: check if the model wanted to call a function
-    if tool_calls:
-        messages.append(response_message)  # extend conversation with assistant's reply
-        # Step 4: send the info for each function call and function response to the model
+            # messages.append(response_message)  # extend conversation with assistant's reply
+            # Step 4: send the info for each function call and function response to the model
         for tool_call in tool_calls:
             function_name = tool_call.function.name
             function_to_call = gpt_tools.available_functions[function_name]
-            print(function_to_call)
+            self.logger.debug(function_to_call)
             function_args = json.loads(tool_call.function.arguments)
             function_response = ""
             if function_name == 'fetch_event_date':
                 # Extract 'date' parameter for get_events function
                 date = function_args.get("date")
                 function_response = function_to_call(date=date)
-                print(function_response)
+                self.logger.debug(function_response)
 
             elif function_name == 'get_events_n_days':
                 # Extract 'n' parameter for get_events function
                 n = function_args.get("n")
                 function_response = function_to_call(n_days=n)
-                print(function_response)
+                self.logger.debug(function_response)
 
             elif function_name == 'create_event':
                 # Extract parameters for add_event function
@@ -163,27 +174,28 @@ def run_conversation(user_input):
                     description=description,
                     location=location
                 )
-                print(function_response)
-            #
+                self.logger.debug(function_response)
+                #
             if function_response:
                 messages.append(
                     {
                         "tool_call_id": tool_call.id,
                         "role": "tool",
                         "name": function_name,
-
                         "content": function_response,
                     }
                 )  # extend conversation with function response
-        second_response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            temperature=0,
-            messages=messages,
-        )  # get a new response from the model where it can see the function response
-        print(second_response)
-        response_message = second_response.choices[0].message
+            second_response = self.client.chat.completions.create(
+                model="gpt-3.5-turbo-1106",
+                temperature=0,
+                messages=messages,
+            )  # get a new response from the model where it can see the function response
 
-        return response_message.content.__str__()
+            messages.append(
+                {
+                    "role": "assistant",
+                    "content": second_response,
+                })
+            self.pretty_print_conversation(messages)
 
-
-run_conversation("what is on my calander tomorrow? ")
+            return messages
